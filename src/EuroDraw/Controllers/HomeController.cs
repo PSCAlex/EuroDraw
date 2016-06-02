37ee300373e682834e61c9446741c3c171dbbddc
PSCAlex <alex@pscpayroll.com>
2016-06-02 16:43:55 +0100

    initial commit

---
+++ src/EuroDraw/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EuroDraw.Models;
+
+namespace EuroDraw.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IDrawRepo _repo;
+
+        public HomeController(IDrawRepo drawRepo)
+        {
+            _repo = drawRepo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Draw()
+        {
+            if (_repo.IsDrawn())
+            {
+                var draw = new DrawViewModel
+                {
+                    countries = _repo.GetCountryList(),
+                    people = _repo.GetPeopleList(),
+                    timeStamp = _repo.GetTimestamp()
+                };
+
+                return View(draw);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        public IActionResult RunDraw()
+        {
+            DateTime d = new DateTime(2016, 6, 2, 15, 43, 0);
+
+            if (!_repo.IsDrawn() && DateTime.Now >= d)
+            {
+                var rnd = new Random();
+                var people = new List<int>();
+                var countries = new List<int>();
+                for (var i = 1; i < 25; i++)
+                {
+                    people.Add(i);
+                    countries.Add(i);
+                }
+
+                var shuffledPeople = people.OrderBy(x => rnd.Next());
+                var shuffledCountries = countries.OrderBy(x => rnd.Next());
+
+                _repo.NewDraw(shuffledPeople, shuffledCountries);
+
+                return View();
+            }else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult PracticeDraw()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ src/EuroDraw/Models/DrawRepo.cs
@@ -0,0 +1,60 @@
+﻿using EuroDraw.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace EuroDraw.Models
+{
+    public interface IDrawRepo
+    {
+        void NewDraw(IEnumerable<int> peopleList, IEnumerable<int> countryList);
+        IEnumerable<int> GetPeopleList();
+        IEnumerable<int> GetCountryList();
+        string GetTimestamp();
+        bool IsDrawn();
+
+    }
+    public class DrawRepo : IDrawRepo
+    {
+        private ApplicationDbContext _context;
+
+        public DrawRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void NewDraw(IEnumerable<int> peopleList,IEnumerable<int> countryList)
+        {
+            var draw = new DrawModel {
+                people = JsonConvert.SerializeObject(peopleList),
+                countries = JsonConvert.SerializeObject(countryList),
+                timeStamp = DateTime.Now
+            };
+
+            _context.Add(draw);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<int> GetPeopleList()
+        {
+            return JsonConvert.DeserializeObject<IEnumerable<int>>(_context.Draw.FirstOrDefault().people);
+        }
+
+        public IEnumerable<int> GetCountryList()
+        {
+            return JsonConvert.DeserializeObject<IEnumerable<int>>(_context.Draw.FirstOrDefault().countries);
+        }
+
+        public string GetTimestamp()
+        {
+            return _context.Draw.FirstOrDefault().timeStamp.TimeOfDay.ToString();
+        }
+
+        public bool IsDrawn()
+        {
+            return _context.Draw.Any();
+        }
+    }
+}
